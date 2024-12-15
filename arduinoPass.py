1e095b5b95e75c7f274aaea2e8213af99c7f27fe
dhruvancvt <73853203+dhruvancvt@users.noreply.github.com>
2024-12-15 14:33:22 -0800

    initial commit

--- arduinoPass.py
+++ arduinoPass.py
@@ -1,2 +1,22 @@
 import serial
+import time
 
+
+def readserial(comport, baudrate, timestamp=False):
+
+    ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
+
+    while True:
+
+        data = ser.readline().decode().strip()
+
+        if data and timestamp:
+            timestamp = time.strftime('%H:%M:%S')
+            print(f'{timestamp} > {data}')
+        elif data:
+            print(data)
+
+
+if __name__ == '__main__':
+
+    readserial('cu.usbmodem1423201', 115200, True)                          # COM port, Baudrate, Show timestamp
