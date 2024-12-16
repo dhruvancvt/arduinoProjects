a9c77c8a7e3ce7f87c5bada7d3b9479aa0214bb9
dhruvancvt <73853203+dhruvancvt@users.noreply.github.com>
2024-12-16 15:28:06 -0800

    easier to understand

--- targetgame/arduinoPassover.py
+++ targetgame/arduinoPassover.py
@@ -1,13 +1,8 @@
 import serial
 
-arduino = serial.Serial('/dev/cu.usbmodem1423201', 115200)
+arduino = serial.Serial('/dev/cu.usbmodem1443201', 115200)
 x = 20
-while x < 20 and x > 0:
-    try:
-        data = arduino.readline().decode('utf-8').strip() #reads data from arduino and strips it to readable formal
-        x, y = map(float, data.split(",")) #formats data for printing
-        print(f"X: {x}, Y: {y}")
-        x-=1
-
-    except ValueError:
-        print(f"Invalid data received: {data}")
+while x < 2000 and x > 0:
+    data = arduino.readline().decode('utf-8').strip() #reads data from arduino and strips it to readable formal
+    print(data)
+    x-=1
